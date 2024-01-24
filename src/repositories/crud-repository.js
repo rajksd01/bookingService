c6cf7f2b76546e97148ec5bb9396bd1480980364
Raj Kasaudhan <rajksd9@gmail.com>
2024-01-24 18:05:30 +0530

    Added Repository CRUD for Booking

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,49 @@
+const { StatusCodes } = require("http-status-codes");
+const AppError = require("../utils/errors/app-errors");
+
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    const response = await this.model.create(data);
+    return response;
+  }
+
+  async destroy(id) {
+    const response = await this.model.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (!response) {
+      throw new AppError("Aeroplane Doesn't exist", StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+
+  async get(data) {
+    const response = await this.model.findByPk(data);
+    if (!response) {
+      throw new AppError("Not Found", StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+  async getAll() {
+    const response = await this.model.findAll();
+    return response;
+  }
+
+  async update(id, data) {
+    const response = await this.model.update(data, { where: { id: id } });
+
+    if (response[0] == 0) {
+      throw new AppError(" Not Found", StatusCodes.NOT_FOUND);
+    }
+
+    return response;
+  }
+}
+
+module.exports = CrudRepository;
