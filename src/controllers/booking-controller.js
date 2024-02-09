2c9ea0960b971c1de71f9aa159b07e24c3680a52
Raj Kasaudhan <rajksd9@gmail.com>
2024-02-09 21:46:03 +0530

    FEAT: BOOKING PAYMENT

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -19,6 +19,22 @@ async function createBooking(req, res) {
   }
 }
 
+async function makePayment(req, res) {
+  try {
+    const response = await BookingServices.makePayment({
+      totalCost: req.body.totalCost,
+      userId: req.body.userId,
+      bookingId: req.body.bookingId,
+    });
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    console.log(error);
+    ErrorResponse.error = error;
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+  }
+}
 module.exports = {
   createBooking,
+  makePayment,
 };
