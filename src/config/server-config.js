19a69f8fa25ddc07f30a5a0cfecbeb73de352a58
Raj Kasaudhan <rajksd9@gmail.com>
2024-02-09 17:24:22 +0530

    FIX: Updated url for flight request

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,4 +4,5 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
+  FLIGHT_REQUEST_URL: process.env.FLIGHT_REQUEST_URL,
 };
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -3,13 +3,14 @@ const db = require("../models");
 
 const axios = require("axios");
 const AppError = require("../utils/errors/app-errors");
+const { ServerConfig } = require("../config/");
 
 async function createBooking(data) {
   return new Promise((resolve, reject) => {
     const result = db.sequelize.transaction(
       async function bookingImplementation(t) {
         const flight = await axios.get(
-          `http://localhost:3000/api/v1/flights/flight/${data.flightId}`
+          `${ServerConfig.FLIGHT_REQUEST_URL}/${data.flightId}`
         );
         const flightData = flight.data.data;
         if (data.numberOfSeats > flightData.totalSeats) {
