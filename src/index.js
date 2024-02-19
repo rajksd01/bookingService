32aec0e444b2458f8a740a375cc78f34e428f320
Raj Kasaudhan <rajksd9@gmail.com>
2024-02-19 22:29:35 +0530

    Updated typo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const { logConfig } = require("./config");
 
 const { ServerConfig } = require("../src/config");
 const apiRoutes = require("./routes");
-
+const CRON = require("./utils/common/cron-jobs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log("listening on port " + ServerConfig.PORT);
+  CRON();
+  await Queue.connectQueue();
   logConfig.log({
     level: "info",
     message: `Running on port ${ServerConfig.PORT} `,
